665b42290514e48c24aedba98d43033534ca8710
paul mannion <mannion.paul@gmail.com>
2013-02-05 14:36:11 +0000

    custom routes for users and statuses

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Treebook::Application.routes.draw do
   devise_for :users
 
-  resources :statuses
+  devise_scope :user do
+    get 'register', to: 'devise/registrations#new', as: :register
+    get 'login', to: 'devise/sessions#new', as: :login
+    get 'logout', to: 'devise/sessions#destroy', as: :logout
+  end
 
+  resources :statuses
+  get 'feed', to: 'statuses#index', as: :feed
+  root :to => 'statuses#index'
 
 
   # The priority is based upon order of creation:
@@ -54,7 +61,7 @@ Treebook::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root :to => 'statuses#index'
+
 
   # See how all your routes lay out with "rake routes"
 
