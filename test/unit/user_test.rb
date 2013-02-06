a06c4837fe78d3f90007d5e12a691dbef235c7ae
paul mannion <mannion.paul@gmail.com>
2013-02-06 14:10:41 +0000

    bug fixed for regular expression, profile controller added

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -21,17 +21,28 @@ class UserTest < ActiveSupport::TestCase
 
   test "a user should have a unique profile name" do
     user = User.new
-    user.profile_name  = users(:jason).profile_name
+    user.profile_name  = users(:paul).profile_name
 
     assert !user.save
     assert !user.errors[:profile_name].empty?
   end
 
-  test "a user should have a profile name without spaces"
-    user = User.new
+  test "a user should have a profile name without spaces" do
+    user = User.new(first_name:"paul", last_name: "mannion", email:"mannion.paul1@gmail.com")
+    user.password = user.password_confirmation = "asdfgw"
+
     user.profile_name = "my profile with spaces"
     assert !user.save
     assert !user.errors[:profile_name].empty?
-    assert user.errors[:profile.name].include?("must be formatted correctly")
+    assert user.errors[:profile_name].include?("must be formatted correctly")
+  end
+
+  test "a user can have a correctly formatted profile name" do
+    user = User.new(first_name:"paul", last_name: "mannion", email:"mannion.paul1@gmail.com")
+    user.password = user.password_confirmation = "asdfgw"
+
+    user.profile_name = 'paulmannion_1'
+    assert user.valid?
+  end
 
 end
