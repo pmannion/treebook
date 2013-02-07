f6a30c2c6b387f7215661f687993f3920f13987e
paul mannion <mannion.paul@gmail.com>
2013-02-07 16:59:51 +0000

    profile page added

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ Treebook::Application.routes.draw do
   get 'feed', to: 'statuses#index', as: :feed
   root :to => 'statuses#index'
 
+  get '/:id', to: 'profiles#show'
+
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -60,6 +62,7 @@ Treebook::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
